9bbac2af69d22a9ea0e3afbbfc69f600898cfefb
Karine <62672837+karinevieira@users.noreply.github.com>
2023-10-08 15:26:08 -0300

    config: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,38 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+Rails.logger.debug "\t Seeding users data..."
+
+[
+  {
+    name: "Isabella Galvão",
+    email: "isabella_galvao@limao.com.br",
+    phone: "+55 (91) 98452-1207",
+    cpf: "178.366.500-90"
+  },
+  {
+    name: "Benício Santos",
+    email: "benicio_santos@jbtc.com",
+    phone: "+55 (91) 99196-6356",
+    cpf: "236.587.600-52"
+  },
+  {
+    name: "Yuri Fogaça",
+    email: "yuri-fogaca95@grupoarteoficio.com.br",
+    phone: "+55 (91) 99811-5475",
+    cpf: "882.758.290-89"
+  },
+  {
+    name: "Rafaela Melo",
+    email: "rafaelacarolinamelo@cielo.com.br",
+    phone: "+55 (91) 98998-4251",
+    cpf: "738.263.260-16"
+  },
+  {
+    name: "Andreia Duarte",
+    email: "andreia.daiane.duarte@oliveiracontabil.com.br",
+    phone: "+55 (91) 99757-6784",
+    cpf: "850.910.480-81"
+  }
+].each do |data|
+  User.create!(data)
+end
