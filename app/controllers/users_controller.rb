18ba01b9b111d9ef69bc72c551a0ab2102998c38
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2023-10-08 10:06:40 -0300

    feat: adds search to find records by any field (#3)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.search(params[:search])
   end
 
   # GET /users/1 or /users/1.json
