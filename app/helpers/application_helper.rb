ea9cf2cb744244b577c3b4a8c79f220a3578b6b3
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2023-10-09 12:46:02 -0300

    style: applies style to feedback messages (#12)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def error_messages(attribute, object)
+    return if object.nil?
+
+    object.errors[attribute].to_sentence.capitalize.presence&.concat(".")
+  end
 end
